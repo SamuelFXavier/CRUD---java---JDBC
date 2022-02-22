62ef35e310b0d629fe68f4d61c073f1c083f6abc
SamuelFXavier <xavierfsamuel@gmail.com>
2022-02-22 11:25:25 -0300

    create

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,20 @@
+
 public class Usuario {
+
+    private static final String SQL_CREATETABLE = "DROP TABLE IF EXISTS USUARIO; CREATE TABLE USUARIO"
+            + "("
+            + "ID INT PRIMARY,"
+            + "NOME VARCHAR(100) NOT NULL,"
+            + "SOBRENOME VARCHAR(100) NOT NULL,"
+            + "IDADE INT NOT NULL,"
+            + "EMAIL VARCHAR(100) NOT NULL"
+            + ")"
+            ;
+
+    // Create
+    private static  final String SQL_INSERT_1 = "INSERT INTO USUARIO (ID, NOME, SOBRENOME, IDADE, EMAIL) VALUES (1, 'Arthur', 'Augusto', 25, 'arthuraugusto@gmail.com')";
+    private static  final String SQL_INSERT_2 = "INSERT INTO USUARIO (ID, NOME, SOBRENOME, IDADE, EMAIL) VALUES (1, 'Pablo', 'Medeiros', 22, 'pablo.medeiros@gmail.com')";
+    private static  final String SQL_INSERT_3 = "INSERT INTO USUARIO (ID, NOME, SOBRENOME, IDADE, EMAIL) VALUES (1, 'Jão', 'Pedro', 19, 'joaopedro01@gmail.com')";
+
+
 }
