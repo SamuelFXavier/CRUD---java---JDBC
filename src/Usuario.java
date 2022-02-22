7cc9ea9b5afdd3bf1582cabff1dc5d655e12d0f0
SamuelFXavier <xavierfsamuel@gmail.com>
2022-02-22 11:52:55 -0300

    CrCreating Connection with H2 database test

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,8 @@
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.Statement;
+
 
 public class Usuario {
 
@@ -28,6 +32,38 @@ public class Usuario {
     private static  final  String SQL_UPDATE_2 = "UPDATE USUARIO SET SOBRENOME = 'Pedro' WHERE USUARIO.ID = 3 ";
 
 
+    public static void main(String[] args) throws Exception{
 
+        Connection connection = null;
 
+        try {
+            connection = getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(SQL_CREATETABLE);
+
+            statement.execute(SQL_INSERT_1);
+            statement.execute(SQL_INSERT_2);
+            statement.execute(SQL_INSERT_3);
+
+            statement.execute(SQL_SELECT);
+
+            statement.execute(SQL_DELETE);
+
+            statement.execute(SQL_UPDATE_1);
+            statement.execute(SQL_UPDATE_2);
+
+            statement.execute(SQL_SELECT);
+
+        }catch(SQLIntegrityConstraintViolationException e){
+            e.printStackTrace();
+        }finally {
+            connection.close();
+        }
+    }
+
+    private static Connection getConnection() throws Exception {
+        Class.forName("org.h2.Driver").getDeclaredConstructor().newInstance();
+        return DriverManager.getConnection("jdbc:h2:~/test", "test","1234");
+    }
 }
